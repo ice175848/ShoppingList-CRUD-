75772292b3a18cdd88ec016014750276c18ddb54
ice175848 <memory509609@gmail.com>
2024-08-06 12:41:04 +0800

    加入專案檔案。

---
+++ Shopping/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Shopping
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            IDTextBox = new TextBox();
+            panel1 = new Panel();
+            QuantityTextBox = new TextBox();
+            PriceTextBox = new TextBox();
+            NameTextBox = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            splitContainer1 = new SplitContainer();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(3, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(420, 383);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            dataGridView1.ColumnHeaderMouseClick += dataGridView1_ColumnHeaderMouseClick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Create";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // IDTextBox
+            // 
+            IDTextBox.Location = new Point(3, 3);
+            IDTextBox.Name = "IDTextBox";
+            IDTextBox.PlaceholderText = "ID";
+            IDTextBox.Size = new Size(100, 23);
+            IDTextBox.TabIndex = 5;
+            IDTextBox.Text = "1";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(QuantityTextBox);
+            panel1.Controls.Add(PriceTextBox);
+            panel1.Controls.Add(NameTextBox);
+            panel1.Controls.Add(IDTextBox);
+            panel1.Location = new Point(84, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(108, 171);
+            panel1.TabIndex = 6;
+            // 
+            // QuantityTextBox
+            // 
+            QuantityTextBox.Location = new Point(3, 135);
+            QuantityTextBox.Name = "QuantityTextBox";
+            QuantityTextBox.PlaceholderText = "Quantity";
+            QuantityTextBox.Size = new Size(100, 23);
+            QuantityTextBox.TabIndex = 8;
+            QuantityTextBox.Text = "2";
+            // 
+            // PriceTextBox
+            // 
+            PriceTextBox.Location = new Point(3, 89);
+            PriceTextBox.Name = "PriceTextBox";
+            PriceTextBox.PlaceholderText = "Price";
+            PriceTextBox.Size = new Size(100, 23);
+            PriceTextBox.TabIndex = 7;
+            PriceTextBox.Text = "10";
+            // 
+            // NameTextBox
+            // 
+            NameTextBox.Location = new Point(3, 46);
+            NameTextBox.Name = "NameTextBox";
+            NameTextBox.PlaceholderText = "Name";
+            NameTextBox.Size = new Size(100, 23);
+            NameTextBox.TabIndex = 6;
+            NameTextBox.Text = "Name";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(3, 32);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Find";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(3, 61);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 8;
+            button3.Text = "Update";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(3, 90);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 9;
+            button4.Text = "Delete";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(3, 119);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 10;
+            button5.Text = "Sort";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(3, 148);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 11;
+            button6.Text = "Print";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(12, 12);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(button1);
+            splitContainer1.Panel1.Controls.Add(button6);
+            splitContainer1.Panel1.Controls.Add(panel1);
+            splitContainer1.Panel1.Controls.Add(button5);
+            splitContainer1.Panel1.Controls.Add(button2);
+            splitContainer1.Panel1.Controls.Add(button4);
+            splitContainer1.Panel1.Controls.Add(button3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(dataGridView1);
+            splitContainer1.Size = new Size(644, 389);
+            splitContainer1.SplitterDistance = 214;
+            splitContainer1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 269);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 12;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(661, 450);
+            Controls.Add(splitContainer1);
+            Name = "Form1";
+            Text = "商品介面";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private TextBox IDTextBox;
+        private Panel panel1;
+        private TextBox QuantityTextBox;
+        private TextBox PriceTextBox;
+        private TextBox NameTextBox;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private SplitContainer splitContainer1;
+        private Label label1;
+    }
+}
---
+++ Shopping/Form1.cs
@@ -0,0 +1,262 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Shopping
+{
+    public partial class Form1 : Form
+    {
+        private ShoppingCart _shoppingCart;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _shoppingCart = new ShoppingCart();
+            _shoppingCart.AddDefaultProducts(); // 添加預設資料
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            dataGridView1.DataSource = _shoppingCart.GetProducts();
+        }
+
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+
+            public Product(int id, string name, decimal price, int quantity)
+            {
+                Id = id;
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+            }
+        }
+
+        public class ShoppingCart
+        {
+            private List<Product> _products;
+
+            public ShoppingCart()
+            {
+                _products = new List<Product>();
+            }
+
+            public void AddProduct(Product product)
+            {
+                _products.Add(product);
+            }
+
+            public List<Product> GetProducts()
+            {
+                return _products;
+            }
+
+            public void DeleteProduct(int? id = null, string name = null)
+            {
+                var product = _products.FirstOrDefault(p => (id == null || p.Id == id) && (name == null || p.Name == name));
+                if (product != null)
+                {
+                    _products.Remove(product);
+                }
+            }
+
+            public void UpdateProduct(int id, string name, decimal price, int quantity)
+            {
+                var product = _products.FirstOrDefault(p => p.Id == id);
+                if (product != null)
+                {
+                    product.Name = name;
+                    product.Price = price;
+                    product.Quantity = quantity;
+                }
+            }
+
+            public void SortProducts(string columnName, SortOrder sortOrder)
+            {
+                switch (columnName)
+                {
+                    case "Id":
+                        _products = (sortOrder == SortOrder.Ascending) ?
+                            _products.OrderBy(p => p.Id).ToList() :
+                            _products.OrderByDescending(p => p.Id).ToList();
+                        break;
+                    case "Name":
+                        _products = (sortOrder == SortOrder.Ascending) ?
+                            _products.OrderBy(p => p.Name).ToList() :
+                            _products.OrderByDescending(p => p.Name).ToList();
+                        break;
+                    case "Price":
+                        _products = (sortOrder == SortOrder.Ascending) ?
+                            _products.OrderBy(p => p.Price).ToList() :
+                            _products.OrderByDescending(p => p.Price).ToList();
+                        break;
+                    case "Quantity":
+                        _products = (sortOrder == SortOrder.Ascending) ?
+                            _products.OrderBy(p => p.Quantity).ToList() :
+                            _products.OrderByDescending(p => p.Quantity).ToList();
+                        break;
+                }
+            }
+
+            public void AddDefaultProducts()
+            {
+                _products.Add(new Product(1, "Apple", 1.20m, 10));
+                _products.Add(new Product(2, "Banana", 0.80m, 20));
+                _products.Add(new Product(3, "Orange", 1.50m, 15));
+                _products.Add(new Product(4, "Mango", 2.00m, 5));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(IDTextBox.Text);
+            string name = NameTextBox.Text;
+            decimal price = Decimal.Parse(PriceTextBox.Text);
+            int quantity = int.Parse(QuantityTextBox.Text);
+
+            var product = new Product(id, name, price, quantity);
+            _shoppingCart.AddProduct(product);
+
+            RefreshDataGrid();
+            IDTextBox.Text = (dataGridView1.RowCount + 1).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int? id = null;
+            if (int.TryParse(IDTextBox.Text, out int idValue))
+            {
+                id = idValue;
+            }
+
+            string name = string.IsNullOrWhiteSpace(NameTextBox.Text) ? null : NameTextBox.Text;
+
+            decimal? price = null;
+            if (decimal.TryParse(PriceTextBox.Text, out decimal priceValue))
+            {
+                price = priceValue;
+            }
+
+            int? quantity = null;
+            if (int.TryParse(QuantityTextBox.Text, out int quantityValue))
+            {
+                quantity = quantityValue;
+            }
+
+            if (id == null && name == null && price == null && quantity == null)
+            {
+                MessageBox.Show("請輸入至少一個查詢條件。");
+                return;
+            }
+
+            var products = _shoppingCart.GetProducts();
+            var query = products.Where(p =>
+                (id == null || p.Id == id) &&
+                (name == null || p.Name == name) &&
+                (price == null || p.Price == price) &&
+                (quantity == null || p.Quantity == quantity)).ToList();
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = products;
+
+            dataGridView1.ClearSelection();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                var product = row.DataBoundItem as Product;
+                if (product != null && query.Contains(product))
+                {
+                    row.Selected = true;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(IDTextBox.Text);
+            string name = NameTextBox.Text;
+            decimal price = Decimal.Parse(PriceTextBox.Text);
+            int quantity = int.Parse(QuantityTextBox.Text);
+
+            _shoppingCart.UpdateProduct(id, name, price, quantity);
+
+            RefreshDataGrid();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int? id = null;
+            if (int.TryParse(IDTextBox.Text, out int idValue))
+            {
+                id = idValue;
+            }
+
+            string name = string.IsNullOrWhiteSpace(NameTextBox.Text) ? null : NameTextBox.Text;
+
+            _shoppingCart.DeleteProduct(id, name);
+
+            RefreshDataGrid();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.RowIndex < dataGridView1.Rows.Count && e.ColumnIndex >= 0)
+            {
+                var selectedRow = dataGridView1.Rows[e.RowIndex];
+
+                if (selectedRow != null)
+                {
+                    IDTextBox.Text = selectedRow.Cells["Id"].Value?.ToString();
+                    NameTextBox.Text = selectedRow.Cells["Name"].Value?.ToString();
+                    PriceTextBox.Text = selectedRow.Cells["Price"].Value?.ToString();
+                    QuantityTextBox.Text = selectedRow.Cells["Quantity"].Value?.ToString();
+                }
+            }
+        }
+
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.ColumnIndex >= 0)
+            {
+                string columnName = dataGridView1.Columns[e.ColumnIndex].Name;
+                SortOrder sortOrder = GetSortOrder(e.ColumnIndex);
+                _shoppingCart.SortProducts(columnName, sortOrder);
+
+                RefreshDataGrid();
+            }
+        }
+
+        private SortOrder GetSortOrder(int columnIndex)
+        {
+            if (dataGridView1.Columns[columnIndex].HeaderCell.SortGlyphDirection == SortOrder.None ||
+                dataGridView1.Columns[columnIndex].HeaderCell.SortGlyphDirection == SortOrder.Descending)
+            {
+                dataGridView1.Columns[columnIndex].HeaderCell.SortGlyphDirection = SortOrder.Ascending;
+                return SortOrder.Ascending;
+            }
+            else
+            {
+                dataGridView1.Columns[columnIndex].HeaderCell.SortGlyphDirection = SortOrder.Descending;
+                return SortOrder.Descending;
+            }
+        }
+
+        private void RefreshDataGrid()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = _shoppingCart.GetProducts();
+            dataGridView1.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 初始化代碼
+            LoadProducts();
+        }
+    }
+}
